cf88d10bf6f40ffd7cf9d2a5356b3d7a8340c8fc
aadrian105 <elamigo515@hotmail.com>
2020-09-18 16:38:27 -0600

    Separa vocales y consonantes de un texto

---
+++ SepararVocales/Scripts/Index.js
@@ -0,0 +1,18 @@
+﻿function SepararTexto() {
+    var texto = document.getElementById('campoTexto').value, vocal = "", conso = "";
+
+    for (var i = 0; i < texto.length; i++)
+        if ("aeiou".indexOf(texto[i].toLowerCase()) == -1) {
+            conso += texto[i];
+            vocal += " ";
+        }
+        else {
+            vocal += texto[i];
+            conso += " ";
+        }
+        
+    
+
+    document.getElementById('vocalesInput').value = vocal;
+    document.getElementById('consonaInput').value = conso;
+}
